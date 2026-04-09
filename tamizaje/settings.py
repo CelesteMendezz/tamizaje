93112b3615c51339c032d2336e9ec561792b0378
Celeste Mendez <vaniacelestemendez@gmail.com>
2026-04-09 00:07:40 -0600

    Fix requirements for PythonAnywhere

--- tamizaje/settings.py
+++ tamizaje/settings.py
@@ -115,7 +115,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'celesteapp',
@@ -124,14 +124,14 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
     }
-} 
-""" DATABASES = {
+}  """
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
- """
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
